e29f373d48b7e8d2645c7637ec283dd6149aa409
WalterCorsini <corsini.walter@gmail.com>
2024-05-22 14:18:40 +0200

    add milestone 3

--- partials/function.php
+++ partials/function.php
@@ -1,7 +1,9 @@
-<?php if(isset($_GET["email"]) && strpos($_GET["email"], '@') && strpos($_GET["email"], '.') ) {
+<?php if(isset($_GET["email"]) && strpos($_GET["email"], '@') && strpos($_GET["email"], '.')) {
         $_SESSION["email"] = $_GET["email"];
-        $value = "password corretta";
+        $value = "<span class='alert-success'>password corretta</span>";
+    } elseif(empty($_GET["email"])){
+        $value = "<span class='alert-danger'>Campo Vuoto</span>";       
     } else {
-        $value = "Errore";       
+        $value = "<span class='alert-danger'>Errore</span>";       
     }
      ?>
